be37f04cbcbf7d773e6103d58993e2782f3923fd
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2026-07-10 15:27:09 +0200

    Avoid deprecated `secp256k1_ec_privkey_tweak_{add,mul}` usage
    These functions have been deprecated since 2019 and removed since
    libsecp256k1 v0.7.0, so use their `secp256k1_ec_seckey_tweak{add,mul}`
    equivalents instead.

--- _cffi_build/secp256k1.h
+++ _cffi_build/secp256k1.h
@@ -133,7 +133,7 @@ int secp256k1_ec_pubkey_create(
     const unsigned char *seckey
 );
 
-int secp256k1_ec_privkey_tweak_add(
+int secp256k1_ec_seckey_tweak_add(
     const secp256k1_context* ctx,
     unsigned char *seckey,
     const unsigned char *tweak
@@ -145,7 +145,7 @@ int secp256k1_ec_pubkey_tweak_add(
     const unsigned char *tweak
 );
 
-int secp256k1_ec_privkey_tweak_mul(
+int secp256k1_ec_seckey_tweak_mul(
     const secp256k1_context* ctx,
     unsigned char *seckey,
     const unsigned char *tweak
--- secp256k1/__init__.py
+++ secp256k1/__init__.py
@@ -319,14 +319,14 @@ class PrivateKey(ECDSA):
         Tweak the current private key by adding a 32 byte scalar
         to it and return a new raw private key composed of 32 bytes.
         """
-        return _tweak_private(self, lib.secp256k1_ec_privkey_tweak_add, scalar)
+        return _tweak_private(self, lib.secp256k1_ec_seckey_tweak_add, scalar)
 
     def tweak_mul(self, scalar):
         """
         Tweak the current private key by multiplying it by a 32 byte scalar
         and return a new raw private key composed of 32 bytes.
         """
-        return _tweak_private(self, lib.secp256k1_ec_privkey_tweak_mul, scalar)
+        return _tweak_private(self, lib.secp256k1_ec_seckey_tweak_mul, scalar)
 
     def ecdsa_sign(self, msg, raw=False, digest=hashlib.sha256,
                    custom_nonce=None):
